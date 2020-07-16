f6b7988ef736164e7698301ea2d9f3912c5a82fa
munnaislam9925 <munnaislam9925@gmail.com>
2020-07-17 00:03:27 +0600

    first update

--- assignment.js
+++ assignment.js
@@ -1,3 +1,16 @@
-function feetToMile(){
-    
+//feet to mile convert start.
+
+var userNumber;
+function feetToMile(userNumber){
+    var result = parseFloat( userNumber*0.00018939);
+    return result;
 }
+console.log(feetToMile(1000) + " Mile");
+
+//feet to mile convert end.
+
+//woodcalculator start.
+function woodCalculator(){
+
+}
+//woodcalculator end.
